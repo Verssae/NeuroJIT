9b33191b74f279af402310670ab10aa4c22bf55a
verssae <sparky@hanyang.ac.kr>
2024-08-26 15:10:22 +0900

    modify dockerfile and png

--- scripts/extract_pickles.sh
+++ scripts/extract_pickles.sh
@@ -2,5 +2,4 @@
 
 echo "Extracting pickles..."
 cat /app/archive/pickles_part_* > /app/pickles.tar.gz && tar --warning=no-unknown-keyword -xzf /app/pickles.tar.gz -C /app
-echo ls /app/data
 echo "Done."
